9bf343fd86127a47274c6f5012ecf2562df71683
Yuichi Tsuji <mailmagazine32@gmail.com>
2012-05-27 23:07:11 +0900

    ・測定温度データのch数を自動判定に変更
    ・外気温の測定chを設定可能に変更（default は最後のch）
    ・printする条件の温度差を設定可能に（default は0.5℃）
    ・データ末尾に入るAlarmなどを削除

--- slimdata.pl
+++ slimdata.pl
@@ -1,8 +1,14 @@
 use strict;
 use warnings;
 
+#--------setting--------
+my $ta = -1; #Taの場所を指定 ch3ならば、ta=3; -1ならば最後のch
+my $diff = 0.5;
+#-----------------------
+
 my $prevdata = 0;
 my @slimdata;
+my $ch_count = 0;
 
 while(<>){
   my $line;
@@ -11,23 +17,39 @@ while(<>){
 
   if($line =~ /^\d+/){
     my @recdata = split(/,/,$line);
+    my $count = 0;
+    
     foreach my $data(@recdata){
-      $data =~ s/^\+//;
+        $data =~ s/^\+//;
     }
-    my $curr_data = $recdata[2] + $recdata[3] + $recdata[4] + $recdata[5] + $recdata[6] - 5*$recdata[7];
-#    my $curr_data = $recdata[2];
-    if(abs($curr_data - $prevdata) > 0.5){#前のデータと比較し0.5℃以内の差ならば追加
-     
-      my $pushdata="";
-      foreach my $data (@recdata){
-        $pushdata = $pushdata."$data,";
-      }
-      $pushdata =~ s/,$//;
+    my $ch_number = 2;
+    my $curr_data = 0;
+    while($ch_number < $ch_count+2){
+        $curr_data += $recdata[$ch_number];
+        $ch_number++;
+    }
+    if($ta == -1){
+       $curr_data -= $recdata[$ch_count+1]*$ch_count;
+    }else{
+        $curr_data -= $recdata[$ta+1]*$ch_count;
+    }
+
+    if(abs($curr_data - $prevdata) > $diff){#前のデータと比較し0.5℃(default)以内の差ならば追加
+    my $pushdata="";
+    $ch_number = 0;
+    while($ch_number < $ch_count+2){#Alarmなどのデータは不要なため削除
+        $pushdata = $pushdata."$recdata[$ch_number], ";
+        $ch_number++;
+    }
+        
+      $pushdata =~ s/, $//;#末尾のカンマを削除
       push(@slimdata,$pushdata);
       $prevdata = $curr_data;
     }
-
-  }else{
+  }elsif($line =~ /^CH\d+/){#温度データのch数をカウント
+	$ch_count++;
+	push(@slimdata,$line);
+  }else{#温度データ以外をそのまま追記
     push(@slimdata,$line);
   }
 }
