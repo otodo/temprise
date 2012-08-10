1df277b19f1ab354649701404b870c7a59e7ecdd
Yuichi Tsuji <mailmagazine32@gmail.com>
2012-08-10 21:35:11 +0900

    + 実測値から換算値へ変換出来るようにプログラムを追加
      $convert = 0 ならば温度換算は行わない。
      それ以外は、温度換算を行う。
    * 上記処理を追加により、処理の重複が出るため、一部をsub へ。
    * モジュール化したため、一部の変数をグローバル変数へ
    
    2012/08/10 21:35

--- slimdata.pl
+++ slimdata.pl
@@ -2,15 +2,18 @@ use strict;
 use warnings;
 
 #--------setting--------
-my $ta = -1; #Taの場所を指定 ch3ならば、ta=3; -1ならば最後のch
-my $diff = 1; #Ch1 - Chn までの合計温度差
+our $ta = -1; #Taの場所を指定 ch3ならば、ta=3; -1ならば最後のch, 0ならばTaなし
+our $diff = 0.5; #Ch1 - Chn までの合計温度差
 # my $ind_diff = 0.5; #個々の温度差
-# my $convert = 40; #温度換算, 0ならば温度換算を行わない
+our $convert = 40; #温度換算, 0ならば温度換算を行わない
 #-----------------------
 
-my $prevdata = 0;
-my @slimdata;
+our $prevdata = 0;
+our @slimdata;
 my $ch_count = 0;
+
+my $err_message = "ta is 0 but convert is not 0";
+die $err_message if(($ta == 0) and ($convert != 0));
 
 while(<>){
   my $line;
@@ -23,35 +26,25 @@ while(<>){
   }elsif($line =~ /^\d+/){
     my @recdata = split(/,/,$line);
     my $count = 0;
-    
+
+    #TaのCHを決定
+    $ta = $ch_count if($ta == -1);
+
     foreach my $data(@recdata){
         $data =~ s/^\+//;
     }
-    my $ch_number = 2;
-    my $curr_data = 0;
-    while($ch_number < $ch_count+2){
-        $curr_data += $recdata[$ch_number];
-        $ch_number++;
-    }
-    if($ta == -1){
-       $curr_data -= $recdata[$ch_count+1]*$ch_count;
-    }elsif($ta == 0){
-        $curr_data = $curr_data;
-    }else{
-        $curr_data -= $recdata[$ta+1]*$ch_count;
-    }
 
-    if(abs($curr_data - $prevdata) > $diff){#前のデータと比較し0.5℃(default)以内の差ならば追加
-      my $pushdata="";
-      $ch_number = 0;
-      while($ch_number < $ch_count+2){#Alarmなどのデータは不要なため削除
-        $pushdata = $pushdata."$recdata[$ch_number], ";
-        $ch_number++;
+    
+    if($convert == 0){#生データでの処理
+      &add_data(\@recdata);
+    }else{#convert値でTaを換算
+      my $roomtemp = $recdata[$ta+1];
+      my $col = 2;
+      while($col <= $ch_count+1){
+        $recdata[$col] = $recdata[$col] + $convert - $roomtemp;
+        $col++;
       }
-        
-      $pushdata =~ s/, $//;#末尾のカンマを削除
-      push(@slimdata,$pushdata);
-      $prevdata = $curr_data;
+      &add_data(\@recdata);
     }
   }else{#温度データ以外をそのまま追記
     push(@slimdata,$line);
@@ -62,42 +55,35 @@ foreach my $eline(@slimdata){
   print "$eline\n";
 }
 
-#   if($line =~ /^\d+/){
-#     my @recdata = split(/,/,$line);
-#     my $count = 0;
-#     
-#     foreach my $data(@recdata){
-#         $data =~ s/^\+//;
-#     }
-#     my $ch_number = 2;
-#     my $curr_data = 0;
-#     while($ch_number < $ch_count+2){
-#         $curr_data += $recdata[$ch_number];
-#         $ch_number++;
-#     }
-#     if($ta == -1){
-#        $curr_data -= $recdata[$ch_count+1]*$ch_count;
-#     }elsif($ta == 0){
-#         $curr_data = $curr_data;
-#     }else{
-#         $curr_data -= $recdata[$ta+1]*$ch_count;
-#     }
-# 
-#     if(abs($curr_data - $prevdata) > $diff){#前のデータと比較し0.5℃(default)以内の差ならば追加
-#     my $pushdata="";
-#     $ch_number = 0;
-#     while($ch_number < $ch_count+2){#Alarmなどのデータは不要なため削除
-#         $pushdata = $pushdata."$recdata[$ch_number], ";
-#         $ch_number++;
-#     }
-#         
-#       $pushdata =~ s/, $//;#末尾のカンマを削除
-#       push(@slimdata,$pushdata);
-#       $prevdata = $curr_data;
-#     }
-#   }elsif($line =~ /^CH\d+/){#温度データのch数をカウント
-# 	$ch_count++;
-# 	push(@slimdata,$line);
-#   }else{#温度データ以外をそのまま追記
-#     push(@slimdata,$line);
-#   }
+sub total_temp{
+  my $recdata = shift;
+  my $ch_number = 2;
+  my $curr_data = 0;
+  while($ch_number < $ch_count+2){#Alarm 列を計算対象から外す
+      $curr_data += $$recdata[$ch_number];
+      $ch_number++;
+  }
+  if($ta == 0){
+    $curr_data = $curr_data;
+  }else{
+      $curr_data -= $$recdata[$ta+1]*$ch_count;
+  }
+return($curr_data);
+}
+
+sub add_data{
+  my $recdata = shift;
+  my $curr_data = &total_temp($recdata);
+  if(abs($curr_data - $prevdata) >= $diff){#前のデータと比較しdiff以内の差ならば追加
+    my $pushdata="";
+    my $ch_number = 0;
+    while($ch_number < $ch_count+2){#Alarmなどのデータは不要なため削除
+      $pushdata = $pushdata."$$recdata[$ch_number], ";
+      $ch_number++;
+    }
+    $pushdata =~ s/, $//;#末尾のカンマを削除
+    push(@slimdata,$pushdata);
+    $prevdata = $curr_data;
+  }
+  # print "$curr_data\n";
+}
